91467e956acb32b9e3f7d4713c19668791e5f4c7
MariovMesquita <mariovmesquita@gmail.com>
2022-01-27 15:16:14 +0000

    Adds cmidimize class implementation

--- QT/MidiMize/cmidimize.cpp
+++ QT/MidiMize/cmidimize.cpp
@@ -3,5 +3,17 @@
 
 CMidiMize::CMidiMize()
 {
+    for (int i=0; i<N_SYNTHS ; i++)
+    {
+        synths[i]=cSynth();
+    }
+}
 
+CMidiMize* CMidiMize::getInstance()
+{
+    if (!instance)
+    {
+        instance = new CMidiMize;
+        return instance;
+    }
 }
--- QT/MidiMize/cmidimize.h
+++ QT/MidiMize/cmidimize.h
@@ -1,12 +1,22 @@
 #ifndef CMIDIMIZE_H
 #define CMIDIMIZE_H
 
+#include "cSynth.h"
+
+#define N_SYNTHS 2
 
 class CMidiMize
 {
-public:
-    CMidiMize();
-private:
+    private:
+        static CMidiMize* instance;
+        cSynth synths [N_SYNTHS];
+        bool somoMode;
+
+        CMidiMize();
+
+    public:
+        static CMidiMize* getInstance();
+        ~CMidiMize();
 
 };
 
