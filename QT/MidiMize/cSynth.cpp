4951e98b89f30ceb11302c64e3955e94c98ebd38
MariovMesquita <mariovmesquita@gmail.com>
2022-01-27 00:09:06 +0000

    Changes cSynth according to new design.

--- QT/MidiMize/cSynth.cpp
+++ QT/MidiMize/cSynth.cpp
@@ -6,10 +6,10 @@ cSynth::cSynth()
 {
     /* Create settings object and pass init values */
     this->FsSettings = new_fluid_settings();
-    fluid_settings_setnum(FsSettings, "synth.sample-rate", initSettings.fs_sampleRate);
-    fluid_settings_setint(FsSettings, "synth.cpu-cores", initSettings.fs_cpuCores);
-    fluid_settings_setstr(FsSettings, "audio.driver", initSettings.fs_audioDriver.c_str());
-    fluid_settings_setstr(FsSettings, "midi.driver", initSettings.fs_midiDriver.c_str());
+    fluid_settings_setnum(FsSettings, "synth.sample-rate", FS_SAMPLE_RATE);
+    fluid_settings_setint(FsSettings, "synth.cpu-cores", FS_CPU_CORES);
+    fluid_settings_setstr(FsSettings, "audio.driver", FS_AUDIO_DRIVER);
+    fluid_settings_setstr(FsSettings, "midi.driver", FS_MIDI_DRIVER);
 
     /* Create synth instance */
     this->FsSynth = new_fluid_synth(this->FsSettings);
@@ -24,36 +24,90 @@ cSynth::cSynth()
 
 }
 
-/* Turns this synth chorus ON or OFF */
-void cSynth::chorusOnOff(bool onOff)
+/*
+ * MIDI Note On
+ * Sends note on (key) event to MIDI channel (chan)
+ * ALL CHANNELS -> chan = -1
+ * Key ->  0 - 127
+ */
+void cSynth::noteOn(int chan, int key, int vel)
 {
-    if(onOff == 1)
-        fluid_synth_set_chorus_on(this->FsSynth, 1);
-
-    else
-        fluid_synth_set_chorus_on(this->FsSynth, 0);
+    fluid_synth_noteon(this->FsSynth, chan, key, vel);
 }
 
-/* Sets this synth chorus according to settings */
-void cSynth::chorusSet()
+/*
+ * MIDI Note OFF
+ * Sends note off (key) to MIDI channel (chan)
+ * ALL CHANNELS -> chan = -1
+ * Key ->  0 - 127
+ * Vel (velocity) -> 0-127 ( 0 = noteoff )
+ */
+void cSynth::noteOff(int chan, int key)
 {
-    fluid_synth_set_chorus(this->FsSynth, this->synthChorus.Nr, this->synthChorus.Lvl, this->synthChorus.Speed, this->synthChorus.Depth, this->synthChorus.WaveType);
+    fluid_synth_noteoff(this->FsSynth, chan, key);
 }
 
-/* Turns this synth reverb ON or OFF */
-void cSynth::reverbOnOff(bool onOff)
+/*
+ * Turns reverb on or off
+ * Sets reverb values
+ * width    0.0 - 100.0
+ * room     0.0 - 1.0
+ * lvl      0.0 - 1.0
+ * damp     0.0 - 1.0
+ */
+void cSynth::setReverb()
 {
-    if(onOff == 1)
+    if(this->reverb.active == 1)
+    {
         fluid_synth_set_reverb_on(this->FsSynth, 1);
-
+    }
     else
+    {
         fluid_synth_set_reverb_on(this->FsSynth, 0);
+    }
+
+    fluid_synth_set_reverb(this->FsSynth, this->reverb.room, this->reverb.damp, this->reverb.width, this->reverb.lvl);
 }
 
-/* Sets this synth reverb according to settings */
-void cSynth::reverbSet()
+/*
+ * Turns reverb on or off
+ * Sets reverb values
+ * nr       0 - 99
+ * lvl      0.0 - 10.0
+ * speed    0.1 - 5.0 Hz
+ * depth    0.0 - 21.0
+ * type     FLUID_CHORUS_MOD_SINE, FLUID_CHORUS_MOD_TRIANGLE
+ */
+void cSynth::setChorus()
 {
-    fluid_synth_set_reverb(this->FsSynth, this->synthReverb.RoomSize, this->synthReverb.Damp, this->synthReverb.Width, this->synthReverb.Lvl);
+    if(this->chorus.active == 1)
+    {
+        fluid_synth_set_chorus_on(this->FsSynth, 1);
+    }
+    else
+    {
+        fluid_synth_set_chorus_on(this->FsSynth, 0);
+    }
+
+    fluid_synth_set_chorus(this->FsSynth, this->chorus.nr, this->chorus.lvl, this->chorus.speed, this->chorus.depth, this->chorus.waveType);
+}
+
+/*
+ * Sets synth gain
+ * range 0.0 - 10.0
+ */
+void cSynth::setGain()
+{
+    fluid_synth_set_gain(this->FsSynth, this->gain);
+}
+
+/*
+ * Sets synth gain
+ * range 0.0 - 10.0
+ */
+void cSynth::setPitch()
+{
+    fluid_synth_pitch_bend(this->FsSynth, -1, this->pitchBend);
 }
 
 cSynth::~cSynth()
--- QT/MidiMize/cSynth.h
+++ QT/MidiMize/cSynth.h
@@ -7,57 +7,76 @@
 
 using namespace std;
 
-enum oscillator
+#define FS_SAMPLE_RATE 44100.0
+#define FS_CPU_CORES 2
+#define FS_AUDIO_DRIVER "alsa"
+#define FS_MIDI_DRIVER "alsa_seq"
+
+//struct init_settings
+//{
+//    float fs_sampleRate = 44100.0;
+//    float fs_cpuCores = 2;
+//    string fs_audioDriver = "alsa";
+//    string fs_midiDriver = "alsa_seq";
+//    string fs_soundFonts[3] = {"/etc/MIDImize/soundFonts/sine.sf2" , "/etc/MIDImize/soundFonts/saw.sf2" , "/etc/MIDImize/soundFonts/triangle.sf2"};
+//};
+
+enum oscillator_t
 {
     SINE,
     SAW,
     TRIANGLE,
 };
 
-struct init_settings
+struct chorus_settings_t
 {
-    float fs_sampleRate = 44100.0;
-    float fs_cpuCores = 2;
-    string fs_audioDriver = "alsa";
-    string fs_midiDriver = "alsa_seq";
-    string fs_soundFonts[3] = {"/etc/MIDImize/soundFonts/sine.sf2" , "/etc/MIDImize/soundFonts/saw.sf2" , "/etc/MIDImize/soundFonts/triangle.sf2"};
+
+    float active;
+    float speed;
+    int nr;
+    float lvl;
+    float depth;
+    float waveType;
 };
 
-struct chorus_settings
+struct reverb_settings_t
 {
-    float chorusSpeed;
-    int chorusNr;
-    float chorusLvl;
-    float chorusDepth;
-    float chorusActive;
-    float chorusWave;
-};
-
-struct reverb_settings
-{
-    bool reverbActive;
-    float reverbWidth;
-    float reverbRoom;
-    float reverbLvl;
-    float reverbDamp;
+    bool active;
+    float width; // 0.0 - 100.0
+    float room;  // 0.0 - 1.0
+    float lvl;   // 0.0 - 1.0
+    float damp;  // 0.0 - 1.0
 };
 
 class cSynth
 {
     private:
-        init_settings initSettings;
+        fluid_audio_driver_t* FsAudioDriver;
+        fluid_midi_driver_t* FsMidiDriver;
         fluid_settings_t* FsSettings;
         fluid_synth_t* FsSynth;
-        float synthGain;
-        chorus_settings synthChorus;
-        reverb_settings synthReverb;
-        oscillator synthOscillator;
+
         bool synthOn;
         int synthID;
+
+
+        chorus_settings_t chorus;
+        reverb_settings_t reverb;
+        oscillator_t oscillator;
+        float gain;
+        int pitchBend;
 
     public:
         cSynth();
         ~cSynth();
+
+        void noteOn(int chan, int key, int vel);
+        void noteOff(int chan, int key);
+        void setReverb();
+        void setChorus();
+        void setOscillator(chorus_settings_t chorus);
+        void setGain();
+        void setPitch();
 
 };
 
